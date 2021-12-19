a270db2f6fec81739ff68c542fec29fa296ee405
Vozmishchev <st_vozm@mail.ru>
2021-12-19 20:55:12 +0300

    Исправление ошибок и тестирование кнопки "1" на кликабельность

--- app/src/main/java/ru/gb/android_lesson2_calc/MainActivity.java
+++ app/src/main/java/ru/gb/android_lesson2_calc/MainActivity.java
@@ -3,12 +3,28 @@ package ru.gb.android_lesson2_calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button oneButton;
+    private TextView infoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        oneButton = findViewById(R.id.one_button);
+        infoTextView = findViewById(R.id.info_text_view);
+
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                infoTextView.setText(infoTextView.getText() + "1");
+            }
+        });
     }
 }
