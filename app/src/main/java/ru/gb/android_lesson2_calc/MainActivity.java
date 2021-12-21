5a1866581fc5d52fe9a6fb0a81c9ece04636a5dd
Vozmishchev <st_vozm@mail.ru>
2021-12-21 23:51:35 +0300

    Добавление метода onNumberClick при нажатии цифровых кнопок. id пока оставлю, вдруг в дальнейшем что то пойдет не так.

--- app/src/main/java/ru/gb/android_lesson2_calc/MainActivity.java
+++ app/src/main/java/ru/gb/android_lesson2_calc/MainActivity.java
@@ -2,29 +2,50 @@ package ru.gb.android_lesson2_calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
     private Button oneButton;
     private TextView infoTextView;
+    private Button clearButton;
+    private Button pointButton;
+    private Button equalityButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        oneButton = findViewById(R.id.one_button);
-        infoTextView = findViewById(R.id.info_text_view);
+        initInfo();
 
-        oneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoTextView.setText(infoTextView.getText() + "1");
-            }
-        });
     }
+
+    public void initInfo() {
+        infoTextView = findViewById(R.id.info_text_view);
+        clearButton = findViewById(R.id.clear_button);
+        pointButton = findViewById(R.id.point_button);
+        equalityButton = findViewById(R.id.equality_button);
+    }
+
+    public void onNumberClick(View view) {
+        Button buttons = (Button) view;
+        infoTextView.append(buttons.getText());
+    }
+
 }
+
+
+
+
+
+
+
